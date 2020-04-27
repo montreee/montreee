a5513352b17dbc0edf55fd761e80457dd619eece
Timon Schelling <timon.schelling@outlook.de>
2020-04-27 13:51:20 +0200

    #7: implement path

--- modules/modules/webui/kotlin/de/contentup/montreee/modules/webui/repository/treee/Path.kt
+++ modules/modules/webui/kotlin/de/contentup/montreee/modules/webui/repository/treee/Path.kt
@@ -1,3 +1,12 @@
 package de.contentup.montreee.modules.webui.repository.treee
 
-class Path
+class Path(val value: String) {
+    constructor(trace: List<String>) : this(trace.joinToString("/"))
+    constructor(vararg trace: String) : this(trace.asList())
+    constructor(parents: List<String>, element: String) : this(*parents.toTypedArray(), element)
+    constructor(vararg parent: String, element: String) : this(parent.asList(), element)
+
+    val trace = value.split("/")
+    val parents = trace.takeWhile { it !== element }
+    val element = trace.last()
+}
