3e3c3ccf665432a22a3ea3313078caff2b05e5fb
Timon Schelling <timon.schelling@outlook.de>
2020-04-27 14:02:06 +0200

    #7: define Repository

--- modules/modules/webui/kotlin/de/contentup/montreee/modules/webui/repository/treee/Repository.kt
+++ modules/modules/webui/kotlin/de/contentup/montreee/modules/webui/repository/treee/Repository.kt
@@ -1,3 +1,7 @@
 package de.contentup.montreee.modules.webui.repository.treee
 
-interface Repository
+interface Repository {
+    fun find(path: Path): Element?
+    fun delete(path: Path): Element?
+    fun insert(element: Element): Path?
+}
