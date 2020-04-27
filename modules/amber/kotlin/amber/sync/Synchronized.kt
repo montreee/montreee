701d063c2c39ff917a8b23d7363d39e19ff023f2
Timon Schelling <timon.schelling@outlook.de>
2020-04-27 15:12:38 +0200

    #7: clean Synchronized

--- modules/amber/kotlin/amber/sync/Synchronized.kt
+++ modules/amber/kotlin/amber/sync/Synchronized.kt
@@ -1,31 +1,6 @@
 package amber.sync
 
-import amber.sync.exec.ExecutorSynchronizedImpl
-import amber.sync.exec.SynchronisationScope
-import amber.sync.exec.SynchronizedExecutor
-import amber.sync.kotlin.KotlinSynchronizedImpl
-import amber.sync.mutex.MutexSynchronizedImpl
-import amber.sync.safeexec.SafeExecutorSynchronizedImpl
-import kotlinx.coroutines.sync.Mutex
-
 interface Synchronized {
     fun <T> synchronized(block: () -> T): T
     suspend fun <T> asyncSynchronized(block: () -> T): T
-}
-
-@Suppress("FunctionName")
-fun Synchronized(
-        syncMode: SyncMode = SyncMode.KOTLIN, mutex: Mutex? = null, syncExecutor: SynchronizedExecutor? = null
-) = when (syncMode) {
-    SyncMode.SAFE_EXECUTOR   -> SafeExecutorSynchronizedImpl()
-    SyncMode.HEAVY_EXECUTOR  -> ExecutorSynchronizedImpl(SynchronizedExecutor(SynchronisationScope()))
-    SyncMode.UNSAFE_EXECUTOR -> if (syncExecutor == null) ExecutorSynchronizedImpl() else ExecutorSynchronizedImpl(
-            syncExecutor
-    )
-    SyncMode.MUTEX           -> if (mutex == null) MutexSynchronizedImpl() else MutexSynchronizedImpl(mutex)
-    SyncMode.KOTLIN          -> KotlinSynchronizedImpl()
-}
-
-enum class SyncMode {
-    HEAVY_EXECUTOR, SAFE_EXECUTOR, UNSAFE_EXECUTOR, MUTEX, KOTLIN
 }
--- modules/modules/webui/kotlin/de/contentup/montreee/modules/webui/repository/treee/impl/InMemoryListRepository.kt
+++ modules/modules/webui/kotlin/de/contentup/montreee/modules/webui/repository/treee/impl/InMemoryListRepository.kt
@@ -6,26 +6,29 @@ import de.contentup.montreee.modules.webui.repository.treee.Path
 import de.contentup.montreee.modules.webui.repository.treee.Repository
 
 class InMemoryListRepository(list: MutableList<Element> = mutableListOf()) : Repository {
+
     private val list = list.sync()
-    override fun find(path: Path): Element? = list.synchronized ret@{
-        return@ret list.find { it.path == path }
+
+    override fun find(path: Path): Element? = list.synchronized {
+        list.find { it.path == path }
     }
 
-    override fun childes(path: Path): List<Element> = list.synchronized ret@{
-        return@ret list.filter { it.path.parent == path }
+    override fun childes(path: Path): List<Element> = list.synchronized {
+        list.filter { it.path.parent == path }
     }
 
-    override fun delete(path: Path): Element? = list.synchronized ret@{
+    override fun delete(path: Path): Element? = list.synchronized func@{
         val element = find(path)
-        if (!list.remove(element)) return@ret null
+        if (!list.remove(element)) return@func null
         list.removeIf { path.isParent(it.path) }
-        return@ret element
+        return@func element
     }
 
-    override fun insert(element: Element): Path? = list.synchronized ret@{
-        if (find(element.path) != null) return@ret null
-        find(element.path.parent) ?: return@ret null
+    override fun insert(element: Element): Path? = list.synchronized func@{
+        if (find(element.path) != null) return@func null
+        find(element.path.parent) ?: return@func null
         list.add(element)
-        return@ret element.path
+        return@func element.path
     }
+
 }
