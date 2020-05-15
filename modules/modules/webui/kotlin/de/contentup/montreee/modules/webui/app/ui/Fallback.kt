e3db65dec7d254a89e1f000f95b944220628a37c
Timon Schelling <timon.schelling@outlook.de>
2020-05-15 23:28:29 +0200

    #25: extract ui functionality to api

--- modules/modules/webui/kotlin/de/contentup/montreee/modules/webui/app/ui/Fallback.kt
+++ modules/modules/webui/kotlin/de/contentup/montreee/modules/webui/app/ui/Fallback.kt
@@ -1,13 +1,14 @@
-package de.contentup.montreee.modules.webui.app
+package de.contentup.montreee.modules.webui.app.ui
 
+import de.contentup.montreee.modules.webui.app.ApplicationContext
+import de.contentup.montreee.modules.webui.app.webuiDataFolder
 import io.ktor.application.ApplicationCall
 import io.ktor.application.call
-import io.ktor.request.path
 import io.ktor.response.respondFile
 import io.ktor.util.pipeline.PipelineContext
 
 suspend fun PipelineContext<Unit, ApplicationCall>.fallback(context: ApplicationContext) {
-    val path = call.request.path()
+    val path = call.parameters.getAll("path")?.joinToString("/") ?: return
     val safePath = if (path.startsWith("/")) path.removePrefix("/") else path
     val file = webuiDataFolder.resolve(safePath)
 
